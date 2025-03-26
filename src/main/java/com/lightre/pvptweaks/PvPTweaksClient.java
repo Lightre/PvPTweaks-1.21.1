f91472bccb1e3ca1eb4a91b9b87f8e6a113222a6
contr <controlys123@gmail.com>
2025-03-26 22:58:41 +0300

    test1

---
+++ src/main/java/com/lightre/pvptweaks/PvPTweaksClient.java
@@ -0,0 +1,10 @@
+package com.lightre.pvptweaks;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PvPTweaksClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
